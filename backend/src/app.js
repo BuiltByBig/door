a690e599885a65f864db5b8f1eea020b3cdfdd18
Dana Woodman <dana@danawoodman.com>
2015-06-28 22:38:11 -0700

    Get static hosting working with backend server.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,16 @@
 var chalk = require('chalk')
 var express = require('express')
+var path = require('path')
 var app = express()
 var listCards = require('./list-cards')
+
+var STATIC_PATH = path.resolve(__dirname, '../../frontend/dist/')
+console.log(chalk.gray('Hosting static files from:', STATIC_PATH))
+app.use(express.static(STATIC_PATH))
 
 app.get('/api/cards', listCards)
 
 var port = process.env.PORT || 3000
-
-console.log(chalk.green('Starting server on port', port))
-
 app.listen(port)
+
+console.log(chalk.green('Server started port', port))
