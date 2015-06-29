734a76bd43170dad6ad9548cab6c55218a6b137f
Dana Woodman <dana@danawoodman.com>
2015-06-29 00:16:02 -0700

    More intelligent webpack dev build.

--- frontend/webpack.development.config.js
+++ frontend/webpack.development.config.js
@@ -1,8 +1,12 @@
 var chalk = require('chalk')
 var config = require('./webpack.config')
 
+require('dotenv').load()
+
 console.log(chalk.green('Using development config for WebPack'))
 
 config.devtool = 'eval'
+config.output.filename = '[name].js'
+config.output.publicPath = process.env.ASSET_HOST
 
 module.exports = config
